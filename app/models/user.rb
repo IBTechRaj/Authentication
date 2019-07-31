4a24e22937d7cff25256ae1f6ea10a76493f9f0b
ibtechraj <raj_shk@rediffmail.com>
2019-07-31 20:31:56 +0530

    done upto sessions helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
 class User < ApplicationRecord
+	before_create :create_user_token
+	 attr_accessor :remember_token
+	 has_secure_password
 
+ def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 
-	has_secure_password
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, Digest::SHA1.hexdigest(remember_token))
+  end	
+
+  def create_user_token
+  remember
+  end
 end
